83e8f8ea342bfe179b96a490087c36d23b8e75d7
DylanStin <23308060610028@cetis61.edu.mx>
2025-10-08 09:58:31 -0500

    commit

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('base.html')
+@app.route("/animal")
+def animal():
+    return render_template('animales.html')
+@app.route("/vehiculos")
+def vehiculos():
+    return render_template('carros.html')
 if __name__ == '__main__':
     app.run(debug=True)
