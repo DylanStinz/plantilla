c1bd67c871d4dcd8ffeaeb75d9e902d5359561f2
DylanStinz <23308060610028@cetis61.edu.mx>
2025-10-16 10:42:14 -0500

    nopo

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request , redirect ,url_for ,flash
+app.config["SECRET_KEY"]="una_clave_muy_larga_y_dificil_de_adivinar"
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -15,5 +16,29 @@ def maravillas():
 @app.route("/acerca")
 def contacto():
     return render_template('acerca.html') 
+@app.route("/crear")
+def crear():
+    return render_template('crear.html') 
+@app.route("/inicio")
+def inicio():
+    return render_template('inicio.html')
+@app.registra("/crear", methods=["POST", "GET"])
+def registrame():
+    error=None
+    if request.method=="POST":
+            nombreCompleto=request.form["nombre"]
+            email =request.form["email"]
+            password=request.form["password"]
+            confirmPassword=request.form["confirmPassword"]
+            fechaNacimiento=request.form["fechaNacimiento"]
+            genero=request.form["genero"]
+            if password != confirmPassword:
+                error="Las contraseñas no coinciden"
+            if error != None:
+                flash(error)
+                return redirect(url_for("crear.html"))
+            else:
+                flash(f"registro exitoso, bienvenido {nombreCompleto}")
+                return render_template("inicio.html")
 if __name__ == '__main__':
     app.run(debug=True)
